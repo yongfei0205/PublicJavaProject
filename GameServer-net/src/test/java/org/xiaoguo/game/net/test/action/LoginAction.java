7303d8632ed221300aca52bf8cf624067f7e661b
xiaoguo822 <i@xiaoguo822.com>
2015-05-11 23:53:08 +0800

    修改gradle文件,并添加测试代码

---
+++ GameServer-net/src/test/java/org/xiaoguo/game/net/test/action/LoginAction.java
@@ -0,0 +1,11 @@
+package org.xiaoguo.game.net.test.action;
+
+import org.xiaoguo.game.net.action.BaseAction;
+
+public class LoginAction extends BaseAction {
+
+	public LoginAction(int cmd) {
+		super(cmd);
+	}
+
+}
